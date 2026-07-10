c1ac67cfd6f417c5627c1033c5990aa22549d518
GUJJI PHANEENDRA KUMAR REDDY <phaneendragujji@gmail.com>
2026-07-10 22:49:41 +0530

    Added JS  Assignment 4 on random, object functions

---
+++ ass4.js
@@ -0,0 +1,20 @@
+
+/* Answer 1 */
+let dice =Math.floor(Math.random()*6)+1;
+console.log(dice);
+/* Answer 2 */
+const car={
+    name:"MarutiSuzuki",
+    model:"MarutiSuzukiDzire",
+    color:"white"
+};
+console.log(car.name);
+/* Answer 3 */
+const person={
+    name:"JaneDoe",
+    age:21,
+    city:"NewJersey"
+};
+person.city="NewYork";
+person.country="UnitedStates";
+console.log(person);
