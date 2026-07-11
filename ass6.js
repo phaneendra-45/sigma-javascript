6763cc93354b7a9bfe460a278cdc81874d64c30b
GUJJI PHANEENDRA KUMAR REDDY <phaneendragujji@gmail.com>
2026-07-11 23:54:02 +0530

    Added JS Assignment 5 on functions

---
+++ ass6.js
@@ -0,0 +1,41 @@
+/*Answer1*/
+let arr=[8,9,10,1,2,3,4,5,6,7];
+let num=5;
+
+function getElements(arr, num) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > num) {
+            console.log(arr[i]);
+        }
+    }
+}
+getElements(arr, num);
+
+/*Answer2*/
+let str="abcdabcdefgggh";
+function getUnique(str) {
+    let ans = "";
+    for (let i = 0; i < str.length; i++) {
+        let currChar = str[i];
+        if (ans.indexOf(currChar) == -1) {
+            ans += currChar;
+        }
+    }
+    return ans;
+}
+getUnique(str);
+
+/*Answer3*/
+let country=["Australia","Germany","UnitedStatesofAmerica"];
+function longestName(country) {
+    let ansIdx = 0;
+    for (let i = 0; i < country.length; i++) {
+        let ansLen = country[ansIdx].length;
+        let currLen = country[i].length;
+        if (currLen > ansLen) {
+            ansIdx = i;
+        }
+    }
+    return country[ansIdx];
+}
+longestName(country);
