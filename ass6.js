b7dc732ac9c5c7714d8468dd3124d9c1640a2b67
GUJJI PHANEENDRA KUMAR REDDY <phaneendragujji@gmail.com>
2026-07-14 22:52:53 +0530

    Added JS Assignment 6 on array average and iseven function

---
+++ ass6.js
@@ -0,0 +1,14 @@
+/* Answer 1 */
+const arrayAverage=(arr)=>{
+    let total=0;
+    for(let number of arr)
+        {
+            total += number;
+        }
+     return total/arr.length;};
+     let arr=[1,2,3,4,5,6];
+     console.log(arrayAverage(arr));
+/* Answer 2 */
+let num=4;
+const isEven=(num)=>num%2==0;
+console.log(isEven(num));
