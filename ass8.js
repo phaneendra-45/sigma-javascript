9d5c83b476e2aaa667b0210ed540e6ac010653c8
GUJJI PHANEENDRA KUMAR REDDY <phaneendragujji@gmail.com>
2026-07-18 23:43:13 +0530

    Updated Js Assignment 8 on DOM

--- ass8.js
+++ ass8.js
@@ -7,3 +7,14 @@ document.querySelector("body").append(button);
 //Ans2
 button.setAttribute("id", "btn");
 input.setAttribute("placeholder", "username");
+//Ans3
+letbtn=document.querySelector("#btn");
+btn.classList.add("btnStyle");
+//Ans4
+let h1=document.createElement("h1");
+h1.innerHTML="<u> DOM Practice </u>";
+document.querySelector("body").append(h1);
+//Ans5
+let p=document.createElement("p");
+p.innerHTML="ApnaCollege<b>Delta</b>Practice";
+document.querySelector("body").append(p);
