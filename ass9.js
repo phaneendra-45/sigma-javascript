12169524e4fdb3cdc42775309f8bfefeb382fb37
GUJJI PHANEENDRA KUMAR REDDY <phaneendragujji@gmail.com>
2026-07-21 22:46:17 +0530

    Updated JS Assignment 9 on Event Listeners

--- ass9.js
+++ ass9.js
@@ -1,3 +1,4 @@
+/* Answer 1 */
 // Mouseout
 element.addEventListener("mouseout", () => {
     console.log("Mouse left the element");
@@ -17,3 +18,21 @@ window.addEventListener("scroll", () => {
 window.addEventListener("load", () => {
     console.log("Page loaded");
 });
+
+/* Answer 2 */
+ let btn = document.createElement("button");
+        btn.innerText = "Click Me";
+        document.body.appendChild(btn);
+
+        btn.addEventListener("click", function () {
+            btn.style.backgroundColor = "green";
+        });
+/* Answer 3 */
+let input = document.getElementById("inp");
+        let h2 = document.getElementById("head");
+
+        input.addEventListener("input", function () {
+            let name = input.value.replace(/[^a-zA-Z ]/g, "");
+            input.value = name;
+            h2.innerText = name;
+        });
