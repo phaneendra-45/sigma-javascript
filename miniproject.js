251758c9cbd2a35bf69655f56f32493bbdbb6dc7
GUJJI PHANEENDRA KUMAR REDDY <phaneendragujji@gmail.com>
2026-07-24 22:28:28 +0530

    Added Features : Button EventListeners & Matching Sequence

--- miniproject.js
+++ miniproject.js
@@ -11,17 +11,51 @@ document.addEventListener("keypress", function() {
         nextlevel();
     }
 });
-function btnflash(randbtn) {
+function gameflash(randbtn) {
 randbtn.classList.add("flash");
 setTimeout(function() {
     randbtn.classList.remove("flash");
 }, 250);
 }
+function userflash(randbtn) {
+randbtn.classList.add("userflash");
+setTimeout(function() {
+    randbtn.classList.remove("userflash");
+}, 250);
+}
 function nextlevel() {
+    playersequence = [];
     level++;
     h2.innerText = `Level ${level}`;
-    let randidx = Math.floor(Math.random() * 3);
+    let randidx = Math.floor(Math.random() * 4);
     let randcolor = btns[randidx];
     let randbtn = document.querySelector(`.${randcolor}`);
-    btnflash(randbtn);
+    gamesequence.push(randcolor);
+    console.log(gamesequence);
+    gameflash(randbtn);
 }
+function btnPress() {
+    console.log(this);
+    let btn = this;
+    userflash(btn);
+
+    usercolor = btn.getAttribute("id");
+    console.log(usercolor);
+    playersequence.push(usercolor);
+    checkAnswer(playersequence.length - 1);
+}
+
+let allbtns = document.querySelectorAll(".btn");
+for (btn of allbtns) {
+    btn.addEventListener("click", btnPress);
+    };
+
+function checkAnswer(idx) {
+        if (playersequence[idx] == gamesequence[idx]) {
+            if (playersequence.length == gamesequence.length) {
+                setTimeout(nextlevel, 1000);
+            }
+        } else {
+            h2.innerText = "Game Over, Press any key to restart";
+        }
+     }
